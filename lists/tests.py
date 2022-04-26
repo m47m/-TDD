06767195913c39ac4407a1a44d31e4ec2a9f5c98
m47m <xm130812@163.com>
2022-04-26 09:37:11 +0800

    重构

--- lists/tests.py
+++ lists/tests.py
@@ -1,10 +1,15 @@
 
-
-
+from cgitb import html
+import imp
+from urllib import response
 from django.http import HttpRequest
 from lists.views import home_page
 from django.test import TestCase
 from django.urls import resolve
+
+from django.template.loader import render_to_string
+
+
 
 # Create your tests here.
 # class SmokeTest(TestCase):
@@ -12,15 +17,28 @@ from django.urls import resolve
 # 		self.assertEqual(1+1,3)
 
 class HomePageTest(TestCase):
-	def test_root_url_resolve_to_home_page_view(self):
-		found = resolve('/')
-		self.assertEqual(found.func,home_page)
+	def test_uses_home_template(self):
+		response = self.client.get('/')
+		self.assertTemplateUsed(response,'home.html')
 
-	def test_home_page_returns_correct_html(self):
-		request = HttpRequest()
-		response = home_page(request)
-		html = response.content.decode('utf8')
-		self.assertTrue(html.startswith('<html>'))
-		self.assertIn('<title>To-Do lists</title>',html)
-		self.assertTrue(html.endswith('</html>'))
-		
+	# def test_root_url_resolve_to_home_page_view(self):
+	# 	found = resolve('/')
+	# 	self.assertEqual(found.func,home_page)
+
+	# def test_home_page_returns_correct_html(self):
+	# 	# request = HttpRequest()
+	# 	# response = home_page(request)
+	# 	# html = response.content.decode('utf8')
+	# 	# self.assertTrue(html.startswith('<html>'))
+	# 	# self.assertIn('<title>To-Do lists</title>',html)
+	# 	# self.assertTrue(html.endswith('</html>'))
+	# 	response = self.client.get('/')
+
+	# 	html = response.content.decode('utf8')
+	# 	self.assertTrue(html.startswith('<html>'))
+	# 	self.assertIn('<title>To-Do lists</title>',html)
+	# 	self.assertTrue(html.strip().endswith('</html>'))
+
+	# 	self.assertTemplateUsed(response,'home.html')
+
+	
