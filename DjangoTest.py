757eeb2c93a600e45578a105f09d0dd3d8d5f405
m47m <xm130812@163.com>
2022-05-10 15:24:47 +0800

    thrid refactoring

--- DjangoTest.py
+++ DjangoTest.py
@@ -1,5 +1,6 @@
 from typing_extensions import Self
 from urllib import response
+#from matplotlib.pyplot import table
 from selenium import webdriver
 import unittest
 import time
@@ -11,44 +12,63 @@ class NewVisitorTest(unittest.TestCase):
 		self.browser = webdriver.Firefox()
 	def tearDown(self):
 		self.browser.quit()
+	
+	def check_for_row_in_list_table(self,row_text):
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text,[row.text for row in rows])
 
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		self.browser.get('http://localhost:8000')
 
-		self.assertIn('To-Do',self.browser.title)
-		#header_text = self.browser.find_element_by_tag_name('h1').text
-		header_text = self.browser.find_element(By.TAG_NAME,'h1').text
-		self.assertIn('To-Do',header_text)
+		# self.assertIn('To-Do',self.browser.title)
+		# #header_text = self.browser.find_element_by_tag_name('h1').text
+		# header_text = self.browser.find_element(By.TAG_NAME,'h1').text
+		# self.assertIn('To-Do',header_text)
+
+
 
 		inputbox = self.browser.find_element_by_id('id_new_item')
-		self.assertEqual(
-			inputbox.get_attribute('placeholder'),
-			'Enter a to-do item'
-		)
+		# self.assertEqual(
+		# 	inputbox.get_attribute('placeholder'),
+		# 	'Enter a to-do item'
+		# )
 
 		# inputbox.send_keys('Buy peacock feathers')
 
 		# inputbox.send_keys(Keys.ENTER)
 
-		inputbox.send_keys('Use Peacock feathers to make a fly')
+		# inputbox.send_keys('Use Peacock feathers to make a fly')
 
-		inputbox.send_keys(Keys.ENTER)
+		# inputbox.send_keys(Keys.ENTER)
 
 
-		time.sleep(1)
+		# time.sleep(1)
 
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
+		# table = self.browser.find_element_by_id('id_list_table')
+		# rows = table.find_elements_by_tag_name('tr')
 		# self.assertTrue(
 		# 	any(row.text == '1: Buy peacock feathers' for row in rows),
 		# 	f"new to-do item did not appear in table. Contents were:\n{table.text}"
 		# )
 
-		self.assertIn('1: Buy peacock feathers',[row.text for row in rows])
-		self.assertIn('2: Use peacock feathers to make a fly',[row.text for row in rows])
+		# self.assertIn('1: Buy peacock feathers',[row.text for row in rows])
+		# self.assertIn('2: Use peacock feathers to make a fly',[row.text for row in rows])
 
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Use peacock feathers to make a fly')
+		inputbox.send_keys(Keys.ENTER)
+		time.sleep(1)
 
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
 
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Use peacock feathers to make a fly')
+		inputbox.send_keys(Keys.ENTER)
+		time.sleep(1)
+
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
+		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 		self.fail('finish the test')
 	
 
