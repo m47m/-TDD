720aa941d92fc70fc8374f8e9939536c25711e72
m47m <xm130812@163.com>
2022-05-31 08:40:06 +0800

    new URL view and template to display lists

--- lists/views.py
+++ lists/views.py
@@ -8,24 +8,15 @@ from lists.models import Item
 # home_page = None
 # Create your views here.
 def home_page(request):
-	# return HttpResponse('<html><title>To-Do lists</title></html>')
-	# if request.method == 'POST':
-	# 	return HttpResponse(request.POST['item_text'])
-
-	# item = Item()
-	# item.text = request.POST.get('item_text','')
-	# item.save()
-
 	if request.method == 'POST':
-		new_item_text = request.POST['item_text']
-		Item.objects.create(text=new_item_text)
-		return redirect('/')
+		#new_item_text = request.POST['item_text']
+		Item.objects.create(text=request.POST['item_text'])
+		return redirect('/lists/the-only-list-in-the-world/')
 
+	#items = Item.objects.all()
+	return render(request,'home.html')
+
+def view_list(request):
+ 	#pass
 	items = Item.objects.all()
-	return render(request,'home.html',{'items':items})
-	# else:
-	# 	new_item_text = ''
-
-	# return render(request,'home.html',{
-	# 	'new_item_text': new_item_text,
-	# })
+	return render(request,'list.html',{'items':items})
